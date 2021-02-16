2c79e1d4c087405d991c1b45803720ad99d2faf5
Valeriy Mashura <dr.mashura@gmail.com>
2021-02-16 22:08:45 +0200

    add debounce

--- src/index.js
+++ src/index.js
@@ -1,24 +1,65 @@
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/Material.css';
 import './sass/styles.scss';
+import { alert, notice, info, success, error } from '@pnotify/core';
+import { defaultModules } from '@pnotify/core';
+// import '@pnotify/bootstrap4/dist/PNotifyBootstrap4.css';
+
+// import * as Confirm from '@pnotify/confirm';
+// import '@pnotify/confirm/dist/PNotifyConfirm.css';
+
+import * as PNotifyBootstrap4 from '@pnotify/bootstrap4';
 import countryCard from '../src/templates/country-card.hbs';
-import './js/fetchCountries.js';
+import countryList from '../src/templates/country-list.hbs';
+// import fetchCountry from './js/fetchCountries.js';
 const debounce = require('lodash.debounce');
 
+// defaultModules.set(Material);
+
 const refs = {
-  form: document.querySelector('.js-form'),
-  input: document.querySelector('input[data-action="input"]'),
-  findBtn: document.querySelector('button[data-action="find"]'),
+  queryString: document.querySelector('.js-input'),
   card: document.querySelector('.country-card'),
 };
 
-refs.form.addEventListener('submit', event => {
-  event.preventDefault();
-  const countryToFind = refs.input.value;
-  fetch(`https://restcountries.eu/rest/v2/name/${countryToFind}`)
-    .then(res => res.json())
-    .then(data => {
-      let [country] = data;
-      console.log(country);
-      const markup = countryCard(country);
-      refs.card.insertAdjacentHTML('beforeend', markup);
-    });
-});
+function resetQuery() {
+  refs.card.innerHTML = '';
+  return;
+}
+
+refs.queryString.addEventListener(
+  'input',
+  debounce(event => {
+    event.preventDefault();
+    const query = refs.queryString.value;
+    if (query) {
+      fetch(`https://restcountries.eu/rest/v2/name/${query}`)
+        .then(res => res.json())
+        .then(data => {
+          if (data.length > 1 && data.length <= 10) {
+            resetQuery();
+            console.log(data);
+            const countryArray = data.map(country => {
+              return country;
+            });
+            console.log(countryArray);
+            refs.card.insertAdjacentHTML(
+              'beforeend',
+              countryList(countryArray),
+            );
+          } else {
+            const country = data.map(country => {
+              return countryCard(country);
+            });
+            resetQuery();
+            refs.card.insertAdjacentHTML('beforeend', country);
+          }
+        })
+        .catch(
+          er => resetQuery(),
+          alert({
+            title: "Can't find such country!",
+          }),
+        );
+    }
+  }, 500),
+);
